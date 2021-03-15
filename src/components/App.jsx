f685be97e3646fa7cf47510c7c650ef43fcb954f
Arigam1 <yebkobvsebolshe@gmail.com>
2021-03-16 03:31:48 +0400

    settings webpack

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./app.less"
+
+const App = () => {
+    return (
+        <div className="app">
+            react rabotaet
+        </div>
+    );
+};
+
+export default App;
