819bffe0ef1968451a5a2735bad3a64421e9939a
Arigam1 <yebkobvsebolshe@gmail.com>
2021-03-17 23:15:06 +0400

    redux redux-thunk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCount } from '../reducers/reposReducer';
 import "./app.less"
 
 const App = () => {
+    const dispatch = useDispatch()
+    const count = useSelector(state => state.repos.count)
+
+    const onCountClick = () => {
+        dispatch(setCount(5))
+    }
+
     return (
         <div className="app">
-            react rabotaet
+            react rabotaet!!!
+            <button onClick={() => onCountClick()}>Count</button>
+            <div>{count}</div>
         </div>
     );
 };
