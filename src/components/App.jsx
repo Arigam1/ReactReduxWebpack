ae2df5bb62782d04ca5b25897f8e86d10ec9b043
Arigam1 <yebkobvsebolshe@gmail.com>
2021-03-18 01:19:08 +0400

    get repos from git + searching

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Route } from 'react-router';
-import { setCount } from '../reducers/reposReducer';
 import "./app.less"
 import Main from './main/Main';
 
 const App = () => {
-    const dispatch = useDispatch()
-
-
 
     return (
         <div className="container">
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRepos } from '../actions/repos';
 import "./main.less"
@@ -7,18 +7,32 @@ import Repo from './repo/Repo';
 const Main = () => {
     const dispatch = useDispatch()
     const repos = useSelector(state => state.repos.items)
+    const isFetching = useSelector(state => state.repos.isFetching)
+    const [searchValue, setSearchValue] = useState("")
 
     useEffect(() => {
         dispatch(getRepos())
     }, [])
 
+    const searchHandler = () => {
+        dispatch(getRepos(searchValue))
+    }
+
     return (
         <div>
-            {repos.map(repo =>
-                <Repo repo={repo} />
-            )}
-        </div>
-    );
+            <div>
+                <input value={searchValue} onChange={(e) => { setSearchValue(e.target.value) }} type="text" placeholder="Input name" className="search-input" />
+                <button onClick={() => { searchHandler() }} className="search-button"> search </button>
+            </div>
+            {
+                isFetching === false
+                    ?
+                    repos.map(repo =>
+                        <Repo key={repo.id} repo={repo} />
+                    )
+                    :
+                    <div className="fetching"></div>}
+        </div>);
 };
 
 export default Main;
